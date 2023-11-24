6bafa3b955946f5ed84a0cb39106aea1b0e11601
KavishPaul <133625640+KavishPaul@users.noreply.github.com>
2023-11-24 11:05:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,9 +12,10 @@ import hotelRoute from './routes/hotels.js'
 import booking1Route from './routes/bookings1.js'
 import flightRoute from './routes/flights.js'
 import booking2Route from './routes/bookings2.js'
+import { verifyAdmin, verifyUser, verifyToken } from './utils/verifyToken.js'
 dotenv.config()
 const app = express()
-const port = process.env.PORT || 8000
+const port = process.env.PORT || 4000
 const corsOptions = {
    origin: true,
    credentials: true
@@ -41,12 +42,12 @@ app.use("/api/v1/auth", authRoute)
 app.use("/api/v1/tours", tourRoute)
 app.use("/api/v1/users", userRoute)
 app.use("/api/v1/review", reviewRoute)
-app.use("/api/v1/booking", bookingRoute)
+app.use("/api/v1/booking",verifyToken, bookingRoute)
 app.use("/api/v1/hotels", hotelRoute)
 app.use("/api/v1/booking1", booking1Route)
 app.use("/api/v1/flights", flightRoute)
 app.use("/api/v1/booking2", booking2Route)
 app.listen(port, () => {
    connect()
-   console.log('server listening on port', port)
+   console.log('server listening on port',port)
 })
